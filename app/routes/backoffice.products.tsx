c44856e3f1a98c3d4227fd152a15b6759fdfc5ce
rrjoson <rrjoson08@gmail.com>
2024-05-27 03:09:57 +0800

    ✨ Add ability to see list of products

---
+++ app/routes/backoffice.products.tsx
@@ -0,0 +1,70 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+
+import { getProductListItems } from "~/models/product.server";
+import { requireUserId } from "~/session.server";
+import { useUser } from "~/utils";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const userId = await requireUserId(request);
+  const productListItems = await getProductListItems({ userId });
+  return json({ productListItems });
+};
+
+export default function ProductsPage() {
+  const data = useLoaderData<typeof loader>();
+  const user = useUser();
+
+  return (
+    <div className="flex h-full min-h-screen flex-col">
+      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+        <h1 className="text-3xl font-bold">
+          <Link to=".">Backoffice - Products</Link>
+        </h1>
+        <p>{user.email}</p>
+        <Form action="/logout" method="post">
+          <button
+            type="submit"
+            className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+          >
+            Logout
+          </button>
+        </Form>
+      </header>
+
+      <main className="flex h-full bg-white">
+        <div data-cy="product-list" className="h-full w-80 border-r bg-gray-50">
+          <Link to="new" className="block p-4 text-xl text-blue-500">
+            + New Product
+          </Link>
+
+          <hr />
+
+          {data.productListItems.length === 0 ? (
+            <p className="p-4">No products yet</p>
+          ) : (
+            <ol>
+              {data.productListItems.map((product) => (
+                <li key={product.id}>
+                  <NavLink
+                    className={({ isActive }) =>
+                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
+                    }
+                    to={product.id}
+                  >
+                    ☕ {product.title}
+                  </NavLink>
+                </li>
+              ))}
+            </ol>
+          )}
+        </div>
+
+        <div className="flex-1 p-6">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
